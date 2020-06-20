b086fb02528dd0231f700fbdf9e27aceafd5a708
Jarosław Łoboda <j.loboda@city-service.pl>
2020-06-20 13:22:35 +0200

    dod

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,30 @@
 
-public class Main {
+	import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-}
+import javax.swing.JFrame;
+
+	public class Main {
+
+		public static void main(String[] args) throws FileNotFoundException {
+			
+			 int c = 1;
+			 
+			JFrame window = new JFrame("Moja pierwsza aplikacja GUI");
+			
+			gui.JList panel = new gui.JList();
+			
+			window.add(panel);
+			
+			window.setLocation(200,  100);
+//			window.setSize(500,500);
+			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			window.setVisible(true);
+			window.pack();
+			
+		
+			
+	        
+		}
+	}
--- src/main/java/gui/JList.java
+++ src/main/java/gui/JList.java
@@ -1,5 +1,67 @@
 package gui;
 
-public class JList {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JColorChooser;
+
+public class JList extends JPanel {
+
+	int c = 1;
+	
+Color  red  = new Color(255, 0, 0);
+
+	public JList() throws FileNotFoundException {
+		setLayout(null); 
+		
+	
+		// label1 i text1 
+		JTextField text1 = new JTextField();
+		text1.setBounds(200,200, 100, 40);
+		add(text1);
+		
+		JLabel label1 = new JLabel("Owoce:");
+		label1.setBounds(100, 200, 60, 30);
+		label1.setOpaque(true);
+		label1.setBackground(Color.RED);
+		label1.setForeground(Color.GREEN);
+		add(label1);
+		
+		// label2 i text2
+		JTextField text2 = new JTextField();
+		text2.setBounds(200,200, 100, 40);
+		add(text2);
+		
+	
+		File plik = new File("src/main/resources/listaowocow.txt"); // obiekt przechowywuj¹ce dane z pliku tekstowego
+        Scanner in = new Scanner(plik); //odczyt danych
+        String zdanie = in.nextLine(); // wczytujemy do zmiennej linie tekstu
+
+        for (int i = 1; i < 4; i++) {
+
+            System.out.println(in.next());
+            c++;	
+        }
+        
+            JList list1 = new gui.JList(plik);
+            
+        }
+	
+	
+	public JList(File plik) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(500, 500);
+	}
 }
